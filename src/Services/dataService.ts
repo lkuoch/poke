ce62e9af70c29edba154f43eeddc8a2a95cd4bb0
lkuoch <lkuoch@users.noreply.github.com>
2020-05-02 12:45:46 +1000

    Update dependencies

--- src/Services/dataService.ts
+++ src/Services/dataService.ts
@@ -5,3 +5,12 @@ export function RetrieveObjectKeysAsArray(obj: Object): Array<string> {
 export function ObjectMapper<T>(obj: Object, fn: (x: string, _1, _2) => any): T {
   return Object.fromEntries(Object.entries(obj).map(([k, v], i) => [k, fn(v, k, i)])) as T;
 }
+
+export function ModuleIsAvailable(path: string): boolean {
+  try {
+    require.resolve(path);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
